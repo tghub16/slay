b427032441cfe2993d5f8da6f5cad2ab9da9464f
Tavin Togia <tavintogia@gmail.com>
2020-11-12 05:00:40 -0800

    Updates

---
+++ precache-manifest.ba9d137c634a5e8e8e21be71fe5a86af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493cd6941beede8c8d36a728a1b61230",
+    "url": "/slay/index.html"
+  },
+  {
+    "revision": "7dbc1e513bef941521cd",
+    "url": "/slay/static/css/main.b160487e.chunk.css"
+  },
+  {
+    "revision": "3ce651ca03e6166a831a",
+    "url": "/slay/static/js/2.279cc0ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slay/static/js/2.279cc0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbc1e513bef941521cd",
+    "url": "/slay/static/js/main.2f18757b.chunk.js"
+  },
+  {
+    "revision": "35fe388fdb8fb1ce2886",
+    "url": "/slay/static/js/runtime-main.67db842e.js"
+  }
+]);
